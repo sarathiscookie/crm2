588c3f98219f43c771c991062775895115c57ba9
Arun <arun@regensburg-it.de>
2016-07-04 12:57:57 +0530

    TPCRM2 - 30 - initial commit

--- app/Http/Controllers/CronjobController.php
+++ app/Http/Controllers/CronjobController.php
@@ -64,4 +64,61 @@ class CronjobController extends Controller
             }
         }
     }
+
+    /**
+     * Cron- send feedback URL to customer for the last event
+     */
+    public function sendFeedbackLink()
+    {
+        $customerObj = new CustomerController();
+        $events = Event::select('events.id', 'vehicles.execution_id', 'events.title', 'begin_at', 'CUS.title as salutation', 'CUS.firstname', 'CUS.lastname', 'CUS.email', 'vehicles.license_plate')
+            ->join('vehicles', 'vehicles.id', '=', 'events.vehicle_id')
+            ->join('customers AS CUS', 'CUS.id', '=', 'events.customer_id')
+            ->whereNull('feedback_at')
+            ->where('begin_at', '<=', Carbon::now()->subDays(7))
+            ->get();
+        if(count($events)>0)
+        {
+            foreach ($events as $event) {
+                $title ='';
+                if($event->salutation>0)
+                    $title = $customerObj->customerTitle[$event->salutation];
+                $vehicle_title = '';
+                $vehicle_informations = DB::connection('fes')
+                    ->select("SELECT av.id, av.tuning_id, av.tpbezeichnung, av.marke_name, av.modell_name, av.marke_alias, av.modell_alias, av.kraftstoff, av.vehicletype_title, CAST(SUBSTRING(av.tpleistung, 'm*([0-9]{1,})') as int) as dimsport_kw, CAST(SUBSTRING(substring(tpleistung from (position('/' in tpleistung)+1)), 'm*([0-9]{1,})') as int) as dimsport_ps, round((CAST(SUBSTRING(av.tpleistung, 'm*([0-9]{1,})') as int)) * 1.359622) as ps_from_dimsport_kw,
+										(select t.motor_id from mainpage.tuning t where av.tuning_id = t.id) as motor_id,
+										(select m.power from mainpage.motor m, mainpage.tuning t where av.tuning_id = t.id and t.motor_id = m.id) as motor_power,
+										(SELECT CASE WHEN (select t.motor_id from mainpage.tuning t where av.tuning_id = t.id) <> NULL THEN (select m.power from mainpage.motor m, mainpage.tuning t where av.tuning_id = t.id and t.motor_id = m.id) ELSE round((CAST(SUBSTRING(av.tpleistung, 'm*([0-9]{1,})') as int)) * 1.359622) END ) as sort_leistung
+									FROM mainpage.ausfuehrung_view_neu av
+									WHERE av.id = '$event->execution_id'");
+
+                foreach ($vehicle_informations as $vehicle_information) {
+                    if ($vehicle_information->motor_power)
+                        $power = $vehicle_information->motor_power;
+                    else
+                        $power = $vehicle_information->ps_from_dimsport_kw;
+                    $vehicle_title = $vehicle_information->marke_name . " " . $vehicle_information->modell_name . " " . $vehicle_information->tpbezeichnung . " " . "mit " . $power . "PS";
+                }
+                $i=0;
+                if ($event->email != '') {
+                    try {
+                        // For testing
+                        //$event->email = 'avb.priya@gmail.com';
+                        Mail::send('emails.eventFeedback', ['event' => $event, 'vehicle' =>$vehicle_title, 'title' =>$title], function ($message) use ($event) {
+                            $message->to($event->email, $event->firstname . ' ' . $event->lastname)
+                                ->subject('Event Feedback');
+                        });
+                    } catch (Exception $e) {
+                        $i++;
+                    }
+                    if($i==0){
+                        Event::where('id', $event->id)->update(['feedback_at' => Carbon::now()]);
+                    }
+                }
+            }
+        }
+    }
+
+
+
 }
---
+++ resources/views/emails/eventFeedback.blade.php
@@ -0,0 +1,11 @@
+Hello @if($title) {{ $title }} @endif {{ title_case($event->firstname) }} {{ title_case($event->lastname) }}
+<br><br>
+We would like to know your feedback on the following event Held on  {{ date('d.m.Y H:i', strtotime($event->begin_at)) }} <br /><br />
+Event details: <br /><br />
+{{ title_case($event->title) }}
+<br><br>
+Vehicle : {{ $vehicle }} <br><br>
+License plate: {{ $event->license_plate }} <br><br>
+
+Please <a href="#">Click here</a> to give your feedback.
+
